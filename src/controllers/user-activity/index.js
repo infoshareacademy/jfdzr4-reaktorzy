365f73eb1903ce4dc8fe8991dae4753e37282e84
Lukasz Szypulewski <szyp86@gmail.com>
2021-10-06 17:47:52 +0200

    add userActivityProvaider

--- src/controllers/user-activity/index.js
+++ src/controllers/user-activity/index.js
@@ -2,12 +2,14 @@ import { DATABASE_URL } from '../../firebase-config'
 
 export const UsersActivityProvider = ({ children }) => {
 
-    fetch(`${DATABASE_URL}/usersActivity`)
+    fetch(`${DATABASE_URL}/usersActivity.json`)
         .then(r => r.json())
         .then(data => {
-            console.log(data)
+            const formattedData = Object.keys(data).map(key => ({ id: key, ...data[key] }));
+            formattedData.map((usersActivity) => {
+                return console.log(usersActivity.id)
+            })
         })
-
     return <>
         {children}
     </>
