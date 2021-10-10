e9997977951147d6c342cb53aeab7d9da17de29f
Aleksandra Kaczmarek <aleksandra.kaczmarek@hotmail.com>
2021-10-10 10:49:04 +0200

    fixed nav

--- src/components/navigation/nav.js
+++ src/components/navigation/nav.js
@@ -7,7 +7,6 @@ import './nav.css';
 export const Navigation = () => {
     return (
     <div className="container, footer">
-        <button className="btn" color="inherit" component={Link} to="/profil">Profil</button>
         <button  className="btn" color="inherit"> 
             <Link className={'navigation-link'} to="/history">History</Link>
         </button>
