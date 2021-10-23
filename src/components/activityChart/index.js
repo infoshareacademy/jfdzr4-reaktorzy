4921c56224aae49e293471538f39a32cd78230b8
Lukasz Szypulewski <szyp86@gmail.com>
2021-10-23 18:28:37 +0200

    date is assigned to day of week

--- src/components/activityChart/index.js
+++ src/components/activityChart/index.js
@@ -1,3 +1,4 @@
+import { NorthWest } from "@mui/icons-material";
 import { useContext, useEffect, useState } from "react";
 import { formatDate } from "../../controllers/get-date/getDate";
 import { UserContext } from "../../controllers/user-context";
@@ -10,20 +11,28 @@ export const ActivityChart = () => {
   const userId = isLoggedIn.uid;
 
   const initialActivityChart = [];
+  const numberDayOfWeek = [7, 1, 2, 3, 4, 5, 6];
+  const now = new Date();
+  const dayOfWeek = numberDayOfWeek[now.getDay()];
   let chartDay = new Date();
   chartDay.getDate();
   for (let day = 0; day < 365; day++) {
-    if (day > 0) {
+    if (day < 7 - dayOfWeek) {
+      chartDay.setDate(chartDay.getDate() + (7 - dayOfWeek));
+    } else if (day === 7 - dayOfWeek) {
+      chartDay = new Date();
+    } else {
       chartDay.setDate(chartDay.getDate() - 1);
     }
     let formatedDate = formatDate(chartDay);
+
     initialActivityChart.push({ date: formatedDate, progress: 0 });
   }
 
   useEffect(() => {
     const updateChart = activityChart.map((day) => {
       const newProgress = userDateProgress.map((dayUser) => {
-        if (dayUser.date == day.date) {
+        if (dayUser.date === day.date) {
           return dayUser.progress;
         } else return 0;
       });
